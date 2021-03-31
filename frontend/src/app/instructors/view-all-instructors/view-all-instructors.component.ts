9d84b4d14d03cf5d2ef2a797facf0ffc6e94fd47
Rishab Bhatt <bhattrishab@gmail.com>
2021-03-31 14:58:11 -0400

    Pushed the new ta position updated design

---
+++ frontend/src/app/instructors/view-all-instructors/view-all-instructors.component.ts
@@ -0,0 +1,43 @@
+import {Component, OnInit} from '@angular/core';
+import {AuthService} from '../../services/auth/auth.service';
+import {UserService} from '../../services/user.service';
+
+@Component({
+    selector: 'app-view-all-instructors',
+    templateUrl: './view-all-instructors.component.html',
+    styleUrls: ['./view-all-instructors.component.scss']
+})
+export class ViewAllInstructorsComponent implements OnInit {
+
+    constructor(public auth: AuthService, public userService: UserService) {
+    }
+
+    public fileName;
+    public successUpload = false;
+    public allInstructors: any;
+
+    ngOnInit(): void {
+        this.getAllInstructors();
+    }
+
+    getAllInstructors = () => this.userService.getInstructors().subscribe(res => {
+        this.allInstructors = res;
+    });
+
+    onFileSelected(event) {
+
+        const file: File = event.target.files[0];
+        if (file) {
+
+            this.fileName = file.name;
+
+            const formData = new FormData();
+
+            formData.append('file', file);
+            this.userService.uploadInstructors(formData).subscribe(res => {
+                this.successUpload = true;
+                this.getAllInstructors();
+            });
+        }
+    }
+}
